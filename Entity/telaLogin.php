584935c088b0179cbc239435f80b8ab89628a9b6
kauan1 <k219594@dac.unicamp.br>
2019-10-31 12:43:43 -0300

    inicio da tela de login

---
+++ Entity/telaLogin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Login</title>
+    <script src="jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="tema.css">
+    <script>
+        $(document).ready(
+            function(){
+                
+            }
+        )
+    </script>
+</head>
+<body>
+    <div id="divlogin">
+        <form action="" method="POST">
+            <p>Login: <input type="text" name="login"></p>
+            <p>Senha: <input type="password" name="senha"></p>
+            <p><input type="submit" name="logar" value="Logar"></p>
+        </form>    
+    </div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,4 @@
+<?php
+require_once "Config.php";
+
+include("Entity/telaLogin.php");
